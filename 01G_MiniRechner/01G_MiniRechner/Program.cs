5f516ca54c8c4f94cfb9cf4d174e22f20d3a862a
ratitt <01auerap@rgtfo-me.it>
2020-04-23 23:30:35 +0200

    Kann addieren und subtrahieren

--- 01G_MiniRechner/01G_MiniRechner/Program.cs
+++ 01G_MiniRechner/01G_MiniRechner/Program.cs
@@ -9,15 +9,43 @@ namespace _01G_MiniRechner
     {
         static void Main(string[] args)
         {
-            Int64 Ergebnis,Zahl1,Zahl2;
+            String RechenOperation;
+            Console.Write("Was möchten Sie machen?\n\n");
+            Console.Write("Addieren(a)  Subtrahieren(s)\n\n");
+            RechenOperation = Console.ReadLine();
+            Console.Clear();
+            switch (RechenOperation)
+                {
+                    case "a":
+                        Addieren();
+                        break;
+                    case "s":
+                        Subtrahieren();
+                        break;
+                }
+            Console.ReadKey();
+        }
+        static void Addieren()
+        {
+            Console.Write("Addieren\n\n");
+            Console.WriteLine("Geben Sie die 1. Zahl ein:\n");
+            Int64 Zahl1 = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("\nGeben Sie die 2. Zahl ein:\n");
+            Int64 Zahl2 = Convert.ToInt64(Console.ReadLine());
+            Int64 Ergebnis = Zahl1 + Zahl2;
+            Console.WriteLine("\nDas Ergebnis ist = {0} ", Ergebnis);
+
+        }
+        static void Subtrahieren() 
+        {
             Console.Write("Subtrahieren (1.Z-2.Z)\n\n");
             Console.WriteLine("Geben Sie die 1. Zahl ein:\n");
-            Zahl1= Convert.ToInt64(Console.ReadLine());
+            Int64 Zahl1 = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("\nGeben Sie die 2. Zahl ein:\n");
-            Zahl2 = Convert.ToInt64(Console.ReadLine());
-            Ergebnis = Zahl1 - Zahl2;
-            Console.WriteLine("\nDas Ergebnis ist = {0} ",Ergebnis);
-            Console.ReadKey();
+            Int64 Zahl2 = Convert.ToInt64(Console.ReadLine());
+            Int64 Ergebnis = Zahl1 - Zahl2;
+            Console.WriteLine("\nDas Ergebnis ist = {0} ", Ergebnis);
+        
         }
     }
 }
